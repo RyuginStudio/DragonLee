1965b14bed1f0956a02b9ac6f7528f3ace86943f
vszed <zyrpg@vip.qq.com>
2018-05-02 09:40:18 +0800

    进入idle state start
    强制动画为idle树第一个动画

--- Scripts/IdleAnimControler.cs
+++ Scripts/IdleAnimControler.cs
@@ -6,9 +6,10 @@ public class IdleAnimControler : StateMachineBehaviour
 {
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
-    //
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetFloat("IdleWay", 0);  //进入状态idle强制刷为第一个动画
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,10 +18,9 @@ public class IdleAnimControler : StateMachineBehaviour
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        Idle.getInstance().doIdleAnm();
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
+    //
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
