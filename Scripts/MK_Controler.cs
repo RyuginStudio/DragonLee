71287244db39a80392cfec756d2f6cffd025cae5
vszed <zyrpg@vip.qq.com>
2018-05-04 13:01:31 +0800

    鼠标图片资源+角色移动

--- Scripts/MK_Controler.cs
+++ Scripts/MK_Controler.cs
@@ -10,6 +10,9 @@ using UnityEngine;
 
 public class MK_Controler : MonoBehaviour
 {
+    //鼠标右键按住
+    public bool holdPressed = false;
+
     // Use this for initialization
     void Start()
     {
@@ -19,20 +22,51 @@ public class MK_Controler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        MouseDragMove();
     }
 
     public void MouseToMove()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))  //鼠标右键限定
         {
+            holdPressed = true;
             //Debug.Log("MouseToMove()");
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
-            Physics.Raycast(ray, out hitInfo);
+            Physics.Raycast(ray, out hitInfo, 1 << LayerMask.NameToLayer("Terrain"));
             //Debug.Log(hitInfo.point);
-            Run.getInstance().RunToPos(hitInfo.point);              //坐标
-            SmoothLookAt.getInstance().Init_Rotate(hitInfo.point);  //朝向
+            if (hitInfo.collider.tag == "Terrain")
+            {
+                Run.getInstance().RunToPos(hitInfo.point);              //坐标
+                SmoothLookAt.getInstance().Init_Rotate(hitInfo.point);  //朝向
+            }
         }
+    }
+
+    public void MouseMoveFinish()
+    {
+        holdPressed = false;
+    }
+
+    public void MouseDragMove()
+    {
+        if (holdPressed && Input.GetKey(KeyCode.Mouse1))
+        {
+            //Debug.Log("MouseDragMove()");
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            Physics.Raycast(ray, out hitInfo, 1 << LayerMask.NameToLayer("Terrain"));
+            //Debug.Log(hitInfo.point);
+            if (hitInfo.collider.tag == "Terrain")
+            {
+                Run.getInstance().RunToPos(hitInfo.point);              //坐标
+                SmoothLookAt.getInstance().Init_Rotate(hitInfo.point);  //朝向
+            }
+        }
+    }
+
+    public void MoveAndAttack()
+    {
+        Debug.Log("Moveatatack");
     }
 }
