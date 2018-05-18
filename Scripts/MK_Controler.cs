54615c8d180c68cf1c9ccc88d1844d8e21b7833b
vszed <zyrpg@vip.qq.com>
2018-05-18 14:15:32 +0800

    镜头控制+视角锁定
    1.鼠标控制镜头移动
    2.空格让视角定位到角色pos

--- Scripts/MK_Controler.cs
+++ Scripts/MK_Controler.cs
@@ -16,13 +16,16 @@ public class MK_Controler : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        //摄像机偏移量
+        offset = trans_character.position - trans_mainCamera.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         MouseToMove();
+        SpaceToLocateSelf();
+        cameraCroll();
     }
 
     public void MouseRightButtonDown()
@@ -68,6 +71,40 @@ public class MK_Controler : MonoBehaviour
                 NormalAttack.getInstance().attackTargetObj = null;  //清空普攻目标
             }
 
+        }
+    }
+
+    public Transform trans_mainCamera;
+    public Transform trans_character;
+    private Vector3 offset;
+    public bool lockView;
+    //按下空格 => Camera定位到角色自身
+    public void SpaceToLocateSelf()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            trans_mainCamera.position = Vector3.Lerp(trans_mainCamera.position, trans_character.position - offset, Time.deltaTime * 100);
+            lockView = true;
+        }
+        else
+        {
+            lockView = false;
+        }
+    }
+
+    //镜头滚动
+    public void cameraCroll()
+    {
+        if (lockView == false) //视角未锁定前提
+        {
+            if (Input.mousePosition.x >= Screen.width - 25)
+                trans_mainCamera.position = new Vector3(trans_mainCamera.position.x + 20 * Time.deltaTime, trans_mainCamera.position.y, trans_mainCamera.position.z);
+            else if (Input.mousePosition.x <= 25)
+                trans_mainCamera.position = new Vector3(trans_mainCamera.position.x - 20 * Time.deltaTime, trans_mainCamera.position.y, trans_mainCamera.position.z);
+            if (Input.mousePosition.y >= Screen.height - 25)
+                trans_mainCamera.position = new Vector3(trans_mainCamera.position.x, trans_mainCamera.position.y, trans_mainCamera.position.z + 20 * Time.deltaTime);
+            else if (Input.mousePosition.y <= 25)
+                trans_mainCamera.position = new Vector3(trans_mainCamera.position.x, trans_mainCamera.position.y, trans_mainCamera.position.z - 20 * Time.deltaTime);
         }
     }
 }
